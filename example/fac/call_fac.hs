866a1a397b425324488e3b7002d641247df009b2
Yutaka Kinjo <yutakakinjyo@gmail.com>
2014-02-21 21:33:42 +0900

    階乗の計算が再帰呼び出しされていなかったので、修正

--- example/fac/call_fac.hs
+++ example/fac/call_fac.hs
@@ -1,11 +1,10 @@
 fac n = 
     if n == 0 
     then 1 
-    else n * (n-1)
+    else n * fac(n-1)
 
-main = do putStrLn "What is 3! ?"
+main = do putStrLn "What is 4! ?"
           x <- readLn
-          if x == fac 3
+          if x == fac 4
           then putStrLn "You're right!"
           else putStrLn "You're wrong!"
-
--- example/fac/fac.hs
+++ example/fac/fac.hs
@@ -4,4 +4,4 @@ fac n = n * fac(n-1)
 main = print (fac 42)
 
 -- same mean
--- fac n = if n == 0 then 1 else n * (n-1)
+-- fac n = if n == 0 then 1 else n * fac(n-1)
